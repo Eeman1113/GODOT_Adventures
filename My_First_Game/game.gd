0a8f58545a698ef095406e5a56d8f0ec74b01c12
Eeman Majumder <54275491+Eeman1113@users.noreply.github.com>
2024-01-20 16:37:39 +0530

    Add files via upload

---
+++ My_First_Game/game.gd
@@ -0,0 +1,20 @@
+extends Node2D
+func _ready():
+	spawn_mob()
+
+func spawn_mob():
+	var new_mob = preload("res://mob.tscn").instantiate()
+	%PathFollow2D.progress_ratio = randf()
+	new_mob.global_position = %PathFollow2D.global_position
+	add_child(new_mob)
+
+
+
+
+func _on_timer_timeout():
+	spawn_mob()
+
+
+func _on_player_health_over():
+	%Game_Over.visible = true
+	get_tree().paused = true
